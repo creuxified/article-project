cea2b9991dc6a9215ebc6d4efdefd02a0249ff00
Arden Prabaswara <ronarden666@gmail.com>
2025-01-01 14:42:57 +0700

    register view removed faculty section

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -58,23 +58,6 @@
                         </div>
                     </div>
                     <div>
-                        <label for="faculty"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Faculty</label>
-                        <select id="faculty"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            wire:model="selectedFaculty">
-                            <option selected>Chose your faculty</option>
-                            @foreach ($faculties as $faculty)
-                            <option value="{{ $faculty->id }}">{{ $faculty->name }}</option>
-                            @endforeach
-                        </select>
-                        <div class="text-red-600 text-sm font-medium">
-                            @error('selectedFaculty')
-                            {{ $message }}
-                            @enderror
-                        </div>
-                    </div>
-                    <div>
                         <label for="password"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                         <input type="password" name="password" id="password" placeholder="••••••••"
