5885a1baee21418eaba816ee36b53ba4212ff86e
Arden Prabaswara <ronarden666@gmail.com>
2025-01-02 14:58:32 +0700

    Scrap Data Navbar Added

---
+++ app/Livewire/ScrapData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ScrapData extends Component
+{
+    public function render()
+    {
+        return view('livewire.scrap-data');
+    }
+}
--- resources/views/components/user-navbar.blade.php
+++ resources/views/components/user-navbar.blade.php
@@ -47,6 +47,9 @@
         <li>
           <a href="/user-database/{{ Auth::user()->username }}" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">User Database</a>
         </li>
+        <li>
+          <a href="/scrap-data/{{ Auth::user()->username }}" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Scrap Data</a>
+        </li>
       @endif
       <li>
         <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Edit Profile</a>
